92347023471479a100fcc8769d10300fa770d531
b2ankit <ankit15ec007@gmail.com>
2020-06-23 23:37:15 +0530

    First commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("Alarm or Beep \a\n");
+    printf("Backspace\b\n");
+    printf("Form feed \f\n");
+    printf("Carriage Return \r\n");
+    printf("Tab \thorizontal\n");
+    printf("Vertical Tab \v\n");
+    printf("Backslash \\ \n");
+    printf("\'Single quotes\'\n");
+    printf("\"Double quotes\"\n");
+    printf("Question\?\n");
+    return 0;
+}
